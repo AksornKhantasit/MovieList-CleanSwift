68828019da8bb4f2584621ffdcd9b13e1863d099
Aksorn Khantasit <aksorn.khan@gmail.com>
2019-09-23 16:38:04 +0700

    add APIManager file

--- MovieList-CleanSwift/Services/APIManager.swift
+++ MovieList-CleanSwift/Services/APIManager.swift
@@ -14,8 +14,12 @@ enum APIError: Error {
 }
 
 class APIManager {
- 
-    func getMovies<T: Codable>(urlString: String, completion: @escaping (Result<T, APIError>) -> Void) {
+    func getMovies(page: Int, sortBy: String, completion: @escaping (Result<Movie, APIError>) -> Void) {
+        let urlString = "http://api.themoviedb.org/3/discover/movie?api_key=328c283cd27bd1877d9080ccb1604c91&primary_release_date.lte=2016-12-31&sort_by=release_date.\(sortBy)&page=\(page)"
+        request(urlString: urlString, completion: completion)
+    }
+    
+    private func request<T: Codable>(urlString: String, completion: @escaping (Result<T, APIError>) -> Void) {
         guard let url = URL(string: urlString) else {
             return
         }
@@ -30,7 +34,7 @@ class APIManager {
                         let values = try JSONDecoder().decode(T.self, from: data)
                         print(values)
                         completion(.success(values))
-                    } catch {
+                    } catch(let error2) {
                         completion(.failure(.invalidJSON))
                     }
                 }
@@ -38,4 +42,29 @@ class APIManager {
         }
         task.resume()
     }
+ 
+//    func getMovies<T: Codable>(urlString: String, completion: @escaping (Result<T, APIError>) -> Void) {
+//        guard let url = URL(string: urlString) else {
+//            return
+//        }
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+//            if let _ = error {
+//                completion(.failure(.invalidData))
+//            } else if let data = data, let response = response as? HTTPURLResponse {
+//                if response.statusCode == 200 {
+//                    do {
+//                        let values = try JSONDecoder().decode(T.self, from: data)
+//                        print(values)
+//                        completion(.success(values))
+//                    } catch {
+//                        completion(.failure(.invalidJSON))
+//                    }
+//                }
+//            }
+//        }
+//        task.resume()
+//    }
+    
 }
