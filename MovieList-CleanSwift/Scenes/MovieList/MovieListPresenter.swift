d3e4c0013736a484789e2315aea2b9bb1b2a322d
Aksorn Khantasit <aksorn.khan@gmail.com>
2019-09-23 18:44:09 +0700

    Change Popularity variables to use two decimal places

--- MovieList-CleanSwift/Scenes/MovieList/MovieListPresenter.swift
+++ MovieList-CleanSwift/Scenes/MovieList/MovieListPresenter.swift
@@ -23,8 +23,8 @@ class MovieListPresenter: MovieListPresenterInterface {
     // NOTE: Format the response from the Interactor and pass the result back to the View Controller. The resulting view model should be using only primitive types. Eg: the view should not need to involve converting date object into a formatted string. The formatting is done here.
     for movie in movies {
         let title = movie.title
-        let popular = "\(movie.popularity)"
-        let rating = "\(0.00000)"
+        let popular = String(format: "%.2f", movie.popularity)
+        let rating = "\(movie.voteAverage)"
         let movieImageURL = movie.posterPath
         let backdropImageURL = movie.backdropPath
         let MovieViewModel = MovieList.GetMovies.ViewModel.MovieViewModel(title: title, popular: popular, rating: rating, movieImage: movieImageURL, backdropImage: backdropImageURL)
