128f906c5f53a4f7d44b3ffd7b7e0b4ab50f4600
Aksorn Khantasit <aksorn.khan@gmail.com>
2019-09-27 16:49:56 +0700

    add MovieListInteractorTests file and MovieListPresenterTests file

--- MovieList-CleanSwiftTests/MovieListInteractorTests.swift
+++ MovieList-CleanSwiftTests/MovieListInteractorTests.swift
@@ -14,7 +14,65 @@ class MovieListInteractorTests: XCTestCase {
   // MARK: - Subject under test
 
   var sut: MovieListInteractor!
+  var movieListWorkerOutputsSpy: MovieListWorkerOutputsSpy!
+  var movieListInteractorOutputsSpy: MovieListInteractorOutputsSpy!
 
+  class MovieListInteractorOutputsSpy: MovieListPresenterInterface {
+    
+    var presentMoviesCalled = false // mispell
+    var presentSelectedIndexCalled = false
+    var setSortCalled = false
+    var pullRefreshCalled = false
+    var reloadCellCalled = false
+    var presentMoviesResponse: MovieList.GetMovies.Response?
+    var presentSelectedIndexResponse: MovieList.SetSelectedIndex.Response?
+    var setSortResponse: MovieList.SetSort.Response?
+    var pullRefreshResponse: MovieList.PullRefresh.Response?
+    var reloadCellResponse: MovieList.ReloadCell.Response?
+    
+    func presentMovies(response: MovieList.GetMovies.Response) {
+      presentMoviesCalled = true
+      presentMoviesResponse = response
+    }
+    
+    func presentSelectedIndex(response: MovieList.SetSelectedIndex.Response) {
+      presentSelectedIndexCalled = true
+      presentSelectedIndexResponse = response
+    }
+    
+    func setSort(response: MovieList.SetSort.Response) {
+      setSortCalled = true
+      setSortResponse = response
+    }
+    
+    func pullRefresh(response: MovieList.PullRefresh.Response) {
+      pullRefreshCalled = true
+      pullRefreshResponse = response
+    }
+    
+    func reloadCell(response: MovieList.ReloadCell.Response) {
+      reloadCellCalled = true
+      reloadCellResponse = response
+    }  
+  }
+  
+  class MovieListWorkerOutputsSpy: MovieListWorker {
+    
+    var getMoviesCalledBySuceess = false
+    var getMoviesCalledByFaild = false
+    var forcesError = false
+    
+    override func getMovies(page: Int, sortBy: String, _ completion: @escaping (Result<Movie, APIError>) -> Void) {
+      if forcesError {
+        getMoviesCalledByFaild = true
+        completion(Result<Movie,APIError>.failure(APIError.invalidData))
+      } else {
+        getMoviesCalledBySuceess = true
+        completion(Result<Movie,APIError>.success(Movie(page: 1, results: [Results(id: 1, popularity: 1, voteCount: 1, voteAverage: 1, title: "test", overview: "test", backdropPath: "/test.jpg", posterPath: "/test.jpg")])))
+      }
+    }
+  }
+  
   // MARK: - Test lifecycle
 
   override func setUp() {
@@ -30,17 +88,135 @@ class MovieListInteractorTests: XCTestCase {
 
   func setupMovieListInteractor() {
     sut = MovieListInteractor()
+    movieListInteractorOutputsSpy = MovieListInteractorOutputsSpy()
+    movieListWorkerOutputsSpy = MovieListWorkerOutputsSpy(store: MovieListStore())
   }
 
   // MARK: - Test doubles
 
   // MARK: - Tests
 
-  func testSomething() {
+  func testGetMoviesShouldAskPresenterToPresentMoviesWithSuccess() {
+    
     // Given
-
+    sut.presenter = movieListInteractorOutputsSpy
+    sut.worker = movieListWorkerOutputsSpy
+    movieListWorkerOutputsSpy.forcesError = false
+    
     // When
-
+    let request = MovieList.GetMovies.Request()
+    sut.getMovies(request: request)
+    
     // Then
+    XCTAssert(movieListWorkerOutputsSpy.getMoviesCalledBySuceess)
+    XCTAssert(movieListInteractorOutputsSpy.presentMoviesCalled)
+  }
+  
+  func testGetMoviesShouldAskPresenterToPresentMoviesWithFailure() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    sut.worker = movieListWorkerOutputsSpy
+    movieListWorkerOutputsSpy.forcesError = true
+    
+    // When
+    let request = MovieList.GetMovies.Request()
+    sut.getMovies(request: request)
+    
+    // Then
+    XCTAssert(movieListWorkerOutputsSpy.getMoviesCalledByFaild)
+    XCTAssert(movieListInteractorOutputsSpy.presentMoviesCalled)
+  }
+  
+  func testGetMoviesShouldAskPresenterToPresentMoviesWithSuccessCaseLoadMore() {
+
+    // Given
+    sut.page = 2
+    sut.presenter = movieListInteractorOutputsSpy
+    sut.worker = movieListWorkerOutputsSpy
+    movieListWorkerOutputsSpy.forcesError = false
+    
+    // When
+    let request = MovieList.GetMovies.Request()
+    sut.getMovies(request: request)
+    
+    // Then
+    XCTAssert(movieListWorkerOutputsSpy.getMoviesCalledBySuceess)
+    XCTAssert(movieListInteractorOutputsSpy.presentMoviesCalled)
+  }
+  
+  func testSetSelectedIDShouldAskPresenterToPresentSelectedIndex() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    
+    // When
+    let request = MovieList.SetSelectedIndex.Request(id: 1)
+    sut.setSelectedID(request: request)
+    
+    // Then
+    XCTAssert(movieListInteractorOutputsSpy.presentSelectedIndexCalled)
+  
+  }
+  
+  func testSetSortShouldAskPresenterToSetSortByASC() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    
+    // When
+    let request = MovieList.SetSort.Request(sortBy: .asc)
+    sut.setSort(request: request)
+    
+    // Then
+    XCTAssert(movieListInteractorOutputsSpy.setSortCalled)
+    
+  }
+  
+  func testSetSortShouldAskPresenterToSetSortByDESC() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    
+    // When
+    let request = MovieList.SetSort.Request(sortBy: .desc)
+    sut.setSort(request: request)
+    
+    // Then
+    XCTAssert(movieListInteractorOutputsSpy.setSortCalled)
+    
+  }
+  
+  func testPullRefreshShouldAskPresenterToPullRefresh() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    
+    // When
+    let request = MovieList.PullRefresh.Request()
+    sut.pullRefresh(request: request)
+    
+    // Then
+    XCTAssert(movieListInteractorOutputsSpy.pullRefreshCalled)
+    
+  }
+  
+  
+  func testReloadCellShouldAskPresenterToReloadCell() {
+    
+    // Given
+    sut.presenter = movieListInteractorOutputsSpy
+    sut.movies = [Results(id: 1, popularity: 1, voteCount: 1, voteAverage: 1, title: "test", overview: "test", backdropPath: "/test.jpg", posterPath: "/test.jpg")]
+    
+    movieListWorkerOutputsSpy.forcesError = false
+    
+    // When
+    let request = MovieList.ReloadCell.Request(movieId: 1)
+    sut.reloadCell(request: request)
+    
+    // Then
+    XCTAssert(movieListInteractorOutputsSpy.reloadCellCalled)
+    XCTAssertEqual(movieListInteractorOutputsSpy.reloadCellResponse?.movieId,1)
+    
   }
 }
--- MovieList-CleanSwiftTests/MovieListPresenterTests.swift
+++ MovieList-CleanSwiftTests/MovieListPresenterTests.swift
@@ -13,23 +13,49 @@ class MovieListPresenterTests: XCTestCase {
 
   // MARK: - Subject under test
 
-  var sut: MovieListPresenter!
-  var movieListPresenterOutputsSpy : MovieListPresenterOutputsSpy!
+  var presenter: MovieListPresenter!
+  var movieListPresenterOutputsSpy: MovieListPresenterOutputsSpy!
 
-  // MARK: - Test lifecycle
-  class MovieListPresenterOutputsSpy : MovieListViewControllerInterface {
+  class MovieListPresenterOutputsSpy: MovieListViewControllerInterface {
+    
     var displayMoviesCalled = false
     var displaySelectedIndexCalled = false
-    var displayMoviesViewModel : MovieList.GetMovies.ViewModel?
+    var displaySortCalled = false
+    var displayPullRefreshCalled = false
+    var displayReloadCellCalled = false
+    var displayMoviesViewModel: MovieList.GetMovies.ViewModel?
+    var displaySelectedIndexViewModel: MovieList.SetSelectedIndex.ViewModel?
+    var displaySortViewModel: MovieList.SetSort.ViewModel?
+    var displayPullRefreshViewModel: MovieList.PullRefresh.ViewModel?
+    var displayReloadCellViewModel: MovieList.ReloadCell.ViewModel?
     
     func displayMovies(viewModel: MovieList.GetMovies.ViewModel){
       displayMoviesCalled = true
       displayMoviesViewModel = viewModel
     }
+    
     func displaySelectedIndex(viewModel: MovieList.SetSelectedIndex.ViewModel){
       displaySelectedIndexCalled = true
+      displaySelectedIndexViewModel = viewModel
+    }
+    
+    func displaySort(viewModel: MovieList.SetSort.ViewModel) {
+      displaySortCalled = true
+      displaySortViewModel = viewModel
+    }
+    
+    func displayPullRefresh(viewModel: MovieList.PullRefresh.ViewModel) {
+      displayPullRefreshCalled = true
+      displayPullRefreshViewModel = viewModel
+    }
+    
+    func displayReloadCell(viewModel: MovieList.ReloadCell.ViewModel) {
+      displayReloadCellCalled = true
+      displayReloadCellViewModel = viewModel
     }
   }
+  
+    // MARK: - Test lifecycle
   
   override func setUp() {
     super.setUp()
@@ -43,7 +69,7 @@ class MovieListPresenterTests: XCTestCase {
   // MARK: - Test setup
 
   func setupMovieListPresenter() {
-    sut = MovieListPresenter()
+    presenter = MovieListPresenter()
     movieListPresenterOutputsSpy = MovieListPresenterOutputsSpy()
     
   }
@@ -51,31 +77,119 @@ class MovieListPresenterTests: XCTestCase {
   // MARK: - Test doubles
 
   // MARK: - Tests
-
-  func testSomething() {
+  
+  func testPresentMovieShouldAskViewControllerToDisplayMoviesWithSuccess() {
     // Given
-    sut.viewController = movieListPresenterOutputsSpy
+    presenter.viewController = movieListPresenterOutputsSpy
     
     // When
-    sut.presentMovies(response: MovieList.GetMovies.Response(movies: [Results(id: 0.0, popularity: 0.0, voteCount: 0.0, voteAverage: 0.0, title: "", overview: "", backdropPath: "", posterPath: "")]))
+    let results = [Results(id: 1, popularity: 1, voteCount: 1, voteAverage: 1, title: "test", overview: "test", backdropPath: "/test.jpg", posterPath: "/test.jpg")]
+    let response = MovieList.GetMovies.Response(result: .success(results))
+    presenter.presentMovies(response: response)
     
     // Then
+    XCTAssert(movieListPresenterOutputsSpy.displayMoviesCalled, "presentMovies() should ask view controller to displayMovies()")
+    if let viewModel = movieListPresenterOutputsSpy.displayMoviesViewModel {
+      switch viewModel.viewModel {
+      case .success(let movieViewModels):
+        XCTAssertEqual(movieViewModels.first?.id, 1)
+        XCTAssertEqual(movieViewModels.first?.title, "test")
+        XCTAssertEqual(movieViewModels.first?.popular, "1.00")
+        XCTAssertEqual(movieViewModels.first?.movieImage?.absoluteString, "https://image.tmdb.org/t/p/original/test.jpg")
+        XCTAssertEqual(movieViewModels.first?.backdropImage?.absoluteString, "https://image.tmdb.org/t/p/original/test.jpg")
+        
+      default:
+        XCTFail()
+      }
+    } else {
+      XCTFail()
+    }
+  }
+
+  func testPresentMovieShouldAskViewControllerToDisplayMoviesWithFailureByInvalidData() {
+    // Given
+    presenter.viewController = movieListPresenterOutputsSpy
+    // When
+    let response = MovieList.GetMovies.Response(result: .failure(APIError.invalidData))
+    presenter.presentMovies(response: response)
+    // Then
     XCTAssert(movieListPresenterOutputsSpy.displayMoviesCalled)
-  XCTAssertEqual(movieListPresenterOutputsSpy.displayMoviesViewModel?.movieViewModels.first?.id, 0)
-    
+    if let viewModel = movieListPresenterOutputsSpy.displayMoviesViewModel {
+      switch viewModel.viewModel {
+      case .failure :
+        break
+      default:
+        XCTFail()
+      }
+    }
+  }
+  
+  func testPresentMovieShouldAskViewControllerToDisplayMoviesWithFailureByInvalidJSON() {
+    // Given
+    presenter.viewController = movieListPresenterOutputsSpy
+    // When
+    let response = MovieList.GetMovies.Response(result: .failure(APIError.invalidJSON))
+    presenter.presentMovies(response: response)
+    // Then
+    XCTAssert(movieListPresenterOutputsSpy.displayMoviesCalled)
+    if let viewModel = movieListPresenterOutputsSpy.displayMoviesViewModel {
+      var isFail = false
+      switch viewModel.viewModel {
+      case .failure :
+        isFail = true
+      default:
+        isFail = false
+      }
+      XCTAssertTrue(isFail)
+    }
+  }
+  
+  func testPresentSelectedIndexShouldAskViewControllerToDisplaySelectedIndex() {
+    // Given
+    presenter.viewController = movieListPresenterOutputsSpy
+    //When
+    let response = MovieList.SetSelectedIndex.Response()
+    presenter.presentSelectedIndex(response: response)
+    // Then
+    XCTAssert(movieListPresenterOutputsSpy.displaySelectedIndexCalled)
     
   }
   
-  func testblablabla() {
-    //given
-    sut.viewController = movieListPresenterOutputsSpy
-    
+  func testSetSortShouldAskViewControllerToDisplaySort() {
+    // Given
+    presenter.viewController = movieListPresenterOutputsSpy
     //When
-    sut.presentSelectedIndex(response: MovieList.SetSelectedIndex.Response())
-    
+    let response = MovieList.SetSort.Response()
+    presenter.SetSort(response: response)
     //Then
-    //XCTAssert(movieListPresenterOutputsSpy.)
+    XCTAssert(movieListPresenterOutputsSpy.displaySortCalled)
+  }
+  
+  func testPullRefresh() {
+    // Given
+    presenter.viewController =  movieListPresenterOutputsSpy
+    //When
+    let response = MovieList.PullRefresh.Response()
+    presenter.pullRefresh(response: response)
+    //Then
+    XCTAssert(movieListPresenterOutputsSpy.displayPullRefreshCalled)
+  }
+  
+  func testReloadCell() {
+    // Given
+    presenter.viewController =  movieListPresenterOutputsSpy
+    //When
+    let response = MovieList.ReloadCell.Response(movie: Results(id: 1, popularity: 1, voteCount: 1, voteAverage: 1, title: "test", overview: "test", backdropPath: "/test.jpg", posterPath: "/test.jpg"), movieId: 1)
+    presenter.reloadCell(response: response)
+    //Then
+    XCTAssert(movieListPresenterOutputsSpy.displayReloadCellCalled)
     
-    
+    if let viewModel = movieListPresenterOutputsSpy.displayReloadCellViewModel {
+      XCTAssertEqual(viewModel.viewModel.id, 1)
+      XCTAssertEqual(viewModel.viewModel.title, "test")
+      XCTAssertEqual(viewModel.viewModel.popular, "1.00")
+      XCTAssertEqual(viewModel.viewModel.movieImage?.absoluteString, "https://image.tmdb.org/t/p/original/test.jpg")
+      XCTAssertEqual(viewModel.viewModel.backdropImage?.absoluteString, "https://image.tmdb.org/t/p/original/test.jpg")
+    }
   }
 }
